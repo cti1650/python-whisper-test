546582e3597f5c3e8e27fb748b614231898d2550
cti1650 <cti1650@gmail.com>
2024-10-30 14:58:21 +0900

    feat: 自動変換機能を追加（mov, m4a）

--- src/sub.py
+++ src/sub.py
@@ -2,6 +2,7 @@ import os
 import glob
 import whisper
 from whisper_utils import WhisperProcessor
+from moviepy.editor import VideoFileClip, AudioFileClip
 from config import WHISPER_CONFIG
 
 def main():
@@ -21,7 +22,25 @@ def main():
         model = whisper.load_model(model_name)
         
         for audio_path in glob.iglob(os.path.join(processor.input_dir, '*')):
-            processor.process_audio_file(audio_path, model, model_name)
+            current_path = os.path.abspath(audio_path)
+            file_extension = os.path.splitext(current_path)[-1].lower()
+
+            # .movファイルを.mp4に変換
+            if file_extension == '.mov':
+                mp4_path = current_path.replace('.mov', '.mp4')
+                with VideoFileClip(current_path) as clip:
+                    clip.write_videofile(mp4_path, codec="libx264")
+                current_path = mp4_path
+                os.remove(audio_path)
+
+            # .m4aファイルを.mp3に変換
+            elif file_extension == '.m4a':
+                mp3_path = current_path.replace('.m4a', '.mp3')
+                with AudioFileClip(current_path) as clip:
+                    clip.write_audiofile(mp3_path)
+                current_path = mp3_path
+                os.remove(audio_path)
+            processor.process_audio_file(current_path, model, model_name)
 
     print("Transcription complete!")
 
